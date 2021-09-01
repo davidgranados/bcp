4252cce32497c076e80d75193461e25a07798c57
David Granados <david.granados@avaldigitallabs.com>
2021-08-31 20:50:22 -0500

    s13c187: Flex Layout - Diferentes Resoluciones

--- src/app/heroes/pages/list/list.component.ts
+++ src/app/heroes/pages/list/list.component.ts
@@ -5,7 +5,13 @@ import { Hero } from '../../interfaces/heroes';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styles: [],
+  styles: [
+    `
+      mat-card {
+        margin-top: 20px;
+      }
+    `,
+  ],
 })
 export class ListComponent implements OnInit {
   heroes: Hero[] = [];
