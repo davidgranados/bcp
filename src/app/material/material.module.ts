5651f15813f4a2ac0b31e8287db0fbb369822f7d
David Granados <david.granados@avaldigitallabs.com>
2021-08-31 20:43:00 -0500

    s13c187: Material Card - Flex Layout

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -8,11 +9,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   exports: [
-    MatSidenavModule,
-    MatToolbarModule,
     MatButtonModule,
+    MatCardModule,
     MatIconModule,
     MatListModule,
+    MatSidenavModule,
+    MatToolbarModule,
   ],
 })
 export class MaterialModule {}
