d243e3e2a338875c22321f8ec9cf9dec73576985
David Granados <david.granados@avaldigitallabs.com>
2021-08-31 21:44:50 -0500

    s13c194: Variables de entorno

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -2,19 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Hero } from '../interfaces/heroes';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroesService {
+  private baseUrl: string = environment.heroesEndpoint;
 
   constructor(private http: HttpClient) {}
 
   getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>('http://localhost:3000/heroes');
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
   }
 
   getById(id: string): Observable<Hero> {
-    return this.http.get<Hero>(`http://localhost:3000/heroes/${id}`);
+    return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`);
   }
 }
